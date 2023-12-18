8ff30aecab014fc71f30f8957ac807531c808685
Yin Yue <prinostaged@gmail.com>
2023-12-18 08:45:06 +0530

    certificate file included in env

--- source/config/config.ts
+++ source/config/config.ts
@@ -1,7 +1,36 @@
+import dotenv from 'dotenv';
+import fs from 'fs';
+import path from 'path';
+import os from 'os';
+
+dotenv.config();
+
+function getEnvVariable(key: string): string {
+    const value = process.env[key];
+    if (value === undefined) {
+        throw new Error(`Environment variable ${key} is not set.`);
+    }
+    return value;
+}
+
+
+const base64EncodedCert = getEnvVariable('SAML_CERT');
+if (!base64EncodedCert) {
+  throw new Error('SAML_CERT_BASE64 environment variable is not set.');
+}
+
+const certContent = Buffer.from(base64EncodedCert, 'base64').toString('utf-8');
+const tempDir = os.tmpdir();
+const tempCertPath = path.join(tempDir, 'saml.pem');
+
+fs.writeFileSync(tempCertPath, certContent);
+console.log(`SAML certificate written to ${tempCertPath}`);
+
+
 const config = {
     saml: {
-        cert: './source/config/saml.pem',
-        entryPoint: `${process.env.ENTRY_POINT}`,
+        cert: tempCertPath,
+        entryPoint: getEnvVariable('ENTRY_POINT'),
         issuer: 'http://localhost:1337',
         options: {
             failureRedirect: '/login',
@@ -13,7 +42,7 @@ const config = {
     },
     session: {
         resave: false,
-        secret: `${process.env.SECRET}`,
+        secret: getEnvVariable('SECRET'),
         saveUninitialized: true
     }
 };
