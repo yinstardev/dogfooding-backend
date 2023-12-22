780a4d92db5c44744193b488833dd01b68a083a5
Yin Yue <prinostaged@gmail.com>
2023-12-22 10:12:07 +0530

    vercel-checking

--- index.ts
+++ index.ts
@@ -33,7 +33,8 @@ app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 
 app.use((req, res, next) => {
-    res.header('Access-Control-Allow-Origin', req.header('origin'));
+    res.header('Access-Control-Allow-Origin', "*");
+    // res.header('Access-Control-Allow-Origin', 'https://example.com');
     res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization');
     res.header('Access-Control-Allow-Credentials', 'true');
     if (req.method == 'OPTIONS') {
