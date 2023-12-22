3adcee69ddc750f96691828bef26305e459cf6f4
Yin Yue <prinostaged@gmail.com>
2023-12-22 14:47:12 +0530

    getting-personalized

--- index.ts
+++ index.ts
@@ -133,19 +133,42 @@ app.get('/healthcheck', (req, res, next) => {
     return res.status(200).json({ messgae: 'Server is runngggging!' });
 });
 
-app.post('/getauthtoken',async (req, res:any) => {
+// app.post('/getauthtoken',async (req, res:any) => {
 
-        const postData = `secret_key=${process.env.SECRET_KEY}&username=${process.env.EMAIL}.com&access_level=FULL`;
+//         const postData = `secret_key=${process.env.SECRET_KEY}&username=${process.env.EMAIL}.com&access_level=FULL`;
+//         const response = await axios.post(`${process.env.BASE_URL}`, postData, {
+//             headers: {
+//                 'Content-Type': 'application/x-www-form-urlencoded',
+//                 'Accept': 'text/plain'
+//             }
+//         });
+
+//         res.status(200).json(response.data);
+
+// });
+
+app.post('/getauthtoken', async (req, res) => {
+    const { username } = req.body;  // Extract username from request body
+
+    if (!username) {
+        return res.status(400).json({ error: 'Username is required' });
+    }
+
+    const postData = `secret_key=${process.env.SECRET_KEY}&username=${username}&access_level=FULL`;
+    try {
         const response = await axios.post(`${process.env.BASE_URL}`, postData, {
             headers: {
                 'Content-Type': 'application/x-www-form-urlencoded',
                 'Accept': 'text/plain'
             }
         });
-
         res.status(200).json(response.data);
-
+    } catch (error) {
+        console.error('Error fetching data:', error);
+        res.status(500).json({ error: 'Internal Server Error' });
+    }
 });
+
 
 app.post('/checkbhai',async(req,res) => {
     
