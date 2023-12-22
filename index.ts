43416eaaa665c7598b40476028a54afde8d40d65
Yin Yue <prinostaged@gmail.com>
2023-12-22 10:33:07 +0530

    vercel-checking

--- index.ts
+++ index.ts
@@ -33,8 +33,15 @@ app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 
 app.use((req, res, next) => {
-    res.header('Access-Control-Allow-Origin', "*");
+    // res.header('Access-Control-Allow-Origin', "*");
     // res.header('Access-Control-Allow-Origin', 'https://example.com');
+    const allowedOrigins = ['https://dog-fooding-application-git-backend-testing-for-api-yinstardev.vercel.app'];
+    const origin = req.headers.origin || 'http://localhost:3000';
+    
+    if (allowedOrigins.includes(origin)) {
+        res.header('Access-Control-Allow-Origin', origin);
+    }
+
     res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization');
     res.header('Access-Control-Allow-Credentials', 'true');
     if (req.method == 'OPTIONS') {
